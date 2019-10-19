493c13a89cda532d086babc95976e4ecff39d0db
Henrik Lievonen <henrik.lievonen@hotmail.com>
2019-10-19 22:39:39 +0300

    Update build config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     devtool: 'source-map',
@@ -22,4 +23,9 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Karaoke Maker 9000',
+        }),
+    ]
 };
