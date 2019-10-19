97951a85cc88593e8488f28df7010659e35490b1
Henrik Lievonen <henrik.lievonen@hotmail.com>
2019-10-19 23:04:26 +0300

    Make timeline more intuitive

--- src/Timeline.tsx
+++ src/Timeline.tsx
@@ -7,13 +7,15 @@ interface Props {
 }
 
 const Timeline: React.FC<Props> = props => {
-    function handleMove(event: React.MouseEvent) {
-        const t = event.currentTarget;
+    function handleMouse(event: React.MouseEvent) {
+        if (!event.button && !event.buttons) return;
+        const rect = event.currentTarget.getBoundingClientRect();
         if (props.onChange) {
-            props.onChange((event.clientX - t.clientLeft) / t.clientWidth * props.length);
+            props.onChange((event.clientX - rect.left) / rect.width * props.length);
         }
     }
-    return <div style={{ width: '100%', height: '30px', background: 'red', position: 'relative' }} onMouseMove={handleMove}>
+
+    return <div style={{ width: '100%', height: '30px', background: 'red', position: 'relative' }} onMouseDown={handleMouse} onMouseMove={handleMouse}>
         <span style={{ height: '100%', width: '1px', background: 'black', left: `${props.time / props.length * 100}%`, position: 'absolute' }} />
     </div>
 };
